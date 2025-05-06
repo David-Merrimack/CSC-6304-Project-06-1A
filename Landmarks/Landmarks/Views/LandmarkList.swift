7dc9059aba55aafe9bc87e6002cf0c56c05f1975
David-Merrimack <ayriyand@merrimack.edu>
2025-05-06 18:43:37 -0400

    Sixth Commit
    Made the list dynamic.

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by David Ayriyan on 5/6/25.
+//
+
+import SwiftUI
+
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+
+#Preview {
+    LandmarkList()
+}
+
